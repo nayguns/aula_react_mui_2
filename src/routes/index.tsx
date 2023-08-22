cddc69f9f1776f8a4a62aed9285977eaca5a53e7
Gustavo Lagares <steenboorg_gu@hotmail.com>
2023-08-21 21:55:36 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,16 @@
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-    const { toggleTheme } = useAppThemeContext();
+    const { toggleTheme } = useAppThemeContext();    
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toggle Theme</Button>} />
+			<Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toggle Theme</Button>} />
 
-            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+			<Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     );
 }
